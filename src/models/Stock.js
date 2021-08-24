f6391de1f89214e4c96de9864fab3f1e8589e61d
marcUsfiLLipe <mf8santos@gmail.com>
2021-08-24 13:48:40 -0300

    Changed stock model: not uppercase on stock.code

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -1,21 +1,20 @@
 const mongoose = require('../config/database')
 
 const StockSchema = new mongoose.Schema(
-	{
-		type: {
-			type: String,
-		},
-		code: {
-			type: String,
-			uppercase: true,
-		},
-		targetPriceTax: {
-			type: Number,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    type: {
+      type: String,
+    },
+    code: {
+      type: String,
+    },
+    targetPriceTax: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 module.exports = mongoose.model('Stock', StockSchema)
